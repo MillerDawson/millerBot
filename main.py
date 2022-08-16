685a3f4c6188000cf39de07f7c32f6e05c457c9f
MillerDawson1 <63493106+MillerDawson@users.noreply.github.com>
2022-08-16 14:15:03 +0000

    added some new auto find features

--- main.py
+++ main.py
@@ -2,8 +2,26 @@
 
 import discord
 import os
+import requests # allows http requests for api
+import json # allows data to be processed correctly from requets
+import random # allows random selecting
 
 client = discord.Client() ## Client should be named bot in future
+prefix = '!!' # bot prefix
+
+sadWords = ["sad", "depressed", "unhappy", "angry", "miserable", "depressing"]
+
+starterEncouragements = [
+  "Cheer up!",
+  "Hang in there.",
+  "You are a great person / bot!"
+]
+
+def getQuote():
+  response = requests.get("https://zenquotes.io/api/random")
+  json_data = json.loads(response.text)
+  quote = json_data[0]['q'] + " - " + json_data[0]['a']
+  return(quote)
 
 @client.event
 async def on_ready():
@@ -14,8 +32,14 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('!!Hello'):
-    await message.channel.send('Hello there :smile:')
+  msg = message.content
+  
+  if msg.startswith(prefix + 'Quote'):
+    quote=getQuote()
+    await message.channel.send(quote + ' :smile:')
+
+  if any(word in msg for word in sadWords):
+    await message.channel.send(random.choice(starterEncouragements))
 
 client.run(os.getenv('KEY')) 
 
